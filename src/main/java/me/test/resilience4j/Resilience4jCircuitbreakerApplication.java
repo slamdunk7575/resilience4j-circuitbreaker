d53a7330c311697c499f75c0678620f79bfb6017
slamdunk7575 <slamdunk7575@gmail.com>
2023-12-04 16:23:54 +0900

    (feat) init commit

---
+++ src/main/java/me/test/resilience4j/Resilience4jCircuitbreakerApplication.java
@@ -0,0 +1,13 @@
+package me.test.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jCircuitbreakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resilience4jCircuitbreakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/test/resilience4j/Resilience4jCircuitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.test.resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jCircuitbreakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
